149f9030dc523d039e471b6b7b27ae8a1db64edd
zhouxiangjun <onepsycho@163.com>
2018-11-26 19:38:03 +0800

    1

--- src/domain.js
+++ src/domain.js
@@ -1,7 +1,7 @@
 // 配置全局接口域名
  const domain = 'http://39.104.239.208:9111';
 const imgDomain = 'http://39.104.239.208';
-# const domain = 'http://test.frp.rongyaotech.com';
+// const domain = 'http://test.frp.rongyaotech.com';
 export default {
  apiUrl:domain,
  apiImgUrl:imgDomain,
